2842d064eb1a504e88e2046b7b0c21a1c9b4544d
ryanbeales <ryanbeales@gmail.com>
2017-03-02 14:59:27 +0000

    Changed the line by line to a regex

--- process.py
+++ process.py
@@ -18,34 +18,25 @@ for stackfile in stackfiles:
     process_start_time, stack_capture_time, capture_count = stackfile.split('_')[1:]
     
     # Open the stack file and get the contents
-    with open(stackfile, 'r') as f:
-        stackdump = f.readlines()
+    with open(stackfiles[0], 'r') as f:
+        stackdump = f.read()
     
-    # Reset our thread based variables, as we're processing line by line rather
-    # than stack by stack. In future we could turn this in to a multiline regex
-    # to capture the stack and process the information as a block.
-    # For now however, we do this.
-    threadline, threadname, threadtype, threadstate = None, None, None, None
-    
-    for line in stackdump:
-        # Each thread name starts with ". Capture the bit between the quotes to get a thread name
-        if line.startswith('"'):
-            threadline = line
-            threadname = line.split('"')[1]
+    # Iterate over each stack found in the file
+    # The regular expression here is finding any line beginning with ", up to two new
+    # lines in a row. But also capturing the text between the quotes as a group. Both
+    # * characters are non-greedy with ?.
+    for stack in re.finditer('^"(.*?)".*?\n\n', stackdump, re.DOTALL|re.MULTILINE):
+        fullstack = stack.group(0)
+        threadname = stack.group(1)
+        
+        # Do the same genericising of the thread name
+        threadtype = threadname.translate(None, '0123456789')
+        
+        # Find the thread state line, but if it doesn't exist, leave it as None
+        threadstate_match = re.search('java.lang.Thread.State: (.*)', fullstack)
+        threadstate = threadstate_match.group(1) if threadstate_match else None
             
-            # Remove any numbers from the thread name to make it a generic type:
-            threadtype = threadname.translate(None, '0123456789')
-        
-        # The state of the thread is on the running line, but it's also on the next line starting with this:
-        if threadline and line.startswith('   java.lang.Thread.State:'):
-            threadstate = line.split(':')[1].strip()
-        
-        # If the line is blank, it means we're finished processing one of the thread stacks
-        if line == '\n' and threadline and threadname and threadtype and threadstate:
-            # Append all the captured data to the data list, then reset all variables for next thread
-            # in the stack.
-            stackdata.append([process_start_time, stack_capture_time, capture_count, threadtype, threadname, threadstate])
-            threadline, threadname, threadtype, threadstate = None, None, None, None
+        stackdata.append([process_start_time, stack_capture_time, capture_count, threadtype, threadname, threadstate])
 
 # After all the stack dumps have been captured, dump all the captured information in to
 # a csv file:
